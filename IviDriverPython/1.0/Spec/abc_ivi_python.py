87c6fbc3c1377e582bb2755969112fde0d4137f4
Joseph Mueller <joseph.mueller@non.keysight.com>
2026-01-22 09:55:53 -0700

    More examples for discussion

--- IviDriverPython/1.0/Spec/abc_ivi_python.py
+++ IviDriverPython/1.0/Spec/abc_ivi_python.py
@@ -49,21 +49,29 @@ class IviUtility(ABC):
   @property
   @abstractmethod
   def driver_version(self) -> str:
+    """Provides a driver version string. This is a version optionally followed by a descriptive string."""
+
+    """Provides a driver version string. This is a version optionally followed by a descriptive string.
+
+    The format of the version shall follow the rules for FileVersion defined in File Versioning followed by an optional string. If the string is present, a space shall separate the version from the string. The string contains additional driver specific version information. Multi-byte characters are not allowed in the string that this property returns. String characters shall be in the range of \x20 - \x7E."""
     pass
 
   @property
   @abstractmethod
   def driver_vendor(self) -> str:
+    """Returns the name of the vendor that supplies the IVI Core driver."""
     pass
 
   @property
   @abstractmethod
   def instrument_manufacturer(self) -> str:
+    """Returns the name of the manufacturer of the instrument. The IVI driver returns the value it queries from the instrument or a string indicating that it cannot query the instrument identity. For instance, "Cannot query from instrument"."""
     pass
 
   @property
   @abstractmethod
   def instrument_model(self) -> str:
+    """Returns the model number or name of the instrument. The IVI driver returns the value it queries from the instrument or a string indicating that it cannot query the instrument identity. For instance, "Cannot query from instrument"."""
     pass
 
   @property
@@ -76,9 +84,15 @@ class IviUtility(ABC):
   def instrument_firmware(self) -> str:
     pass
 
+  # The query_instrument_status_enabled property although always preset, need only be implemented if it is meaningful for the instrument being controlled. It controls whether the driver queries the instrument status after each operation.
   @property
   @abstractmethod
   def query_instrument_status_enabled(self) -> bool:
+    """IVI drivers shall implement Query Instrument Status Enabled if possible. At initialization Query Instrument Status Enabled shall be false, unless initialization options have been used to enable it.
+
+    If the instrument can be queried for its status and Query Instrument Status Enabled is True, then the driver checks the instrument status at the end of every call by the user to a method that accesses the instrument and reports an error if the instrument has detected an error. If False, the driver does not query the instrument status at the end of each user operation.
+
+    If the instrument status cannot be meaningfully queried after an operation, then this property has no effect on driver operation."""
     pass
 
   @property
@@ -90,6 +104,13 @@ class IviUtility(ABC):
   def error_query(self) -> ErrorQueryResult | None:
     """Returns the last error in the instrument's error queue.
     Returns None if no error is present."""
+    """The Error Query member queries the instrument and returns instrument specific error information.
+
+    For instruments that implement an error queue, Error Query extracts a single error from the queue.
+
+    For instruments that have status registers but no error queue, the IVI driver returns an error consistent with instrument design.
+
+    The operation of Error Query is not affected by the value of Query Instrument Status Enabled."""
     pass
 
   @abstractmethod
